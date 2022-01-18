6b92f52a09b76988145ce5f0f97b96f28f70e34c
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2022-01-18 18:23:20 +0300

    DoneWithoutExtraFiles

--- app/src/main/java/com/example/androidcourseproject/ui/MainActivity.java
+++ app/src/main/java/com/example/androidcourseproject/ui/MainActivity.java
@@ -1,60 +1,33 @@
 package com.example.androidcourseproject.ui;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.example.androidcourseproject.R;
-import com.example.androidcourseproject.databinding.FragmentLivingsBinding;
-import com.example.androidcourseproject.kal.DbHelper;
-import com.example.androidcourseproject.room.AdditionalServicesRoom;
-import com.example.androidcourseproject.room.ApartmentRoom;
 import com.example.androidcourseproject.room.AppDatabase;
-import com.example.androidcourseproject.room.ClientRoom;
 import com.example.androidcourseproject.room.DiscountRoom;
-import com.example.androidcourseproject.room.LivingRoom;
-import com.example.androidcourseproject.ui.clients.ClientsFragment;
 import com.example.androidcourseproject.ui.clients.ClientsViewModel;
-import com.example.androidcourseproject.ui.livings_and_bookings.LivingsAndBookingsFragment;
 import com.example.androidcourseproject.ui.livings_and_bookings.PagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.androidcourseproject.databinding.ActivityMainBinding;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ActivityMainBinding binding;
-    SQLiteDatabase database;
-    DbHelper dbHelper;
-    SimpleCursorAdapter simpleCursorAdapter;
     public static AppDatabase db;
     static NavController navController;
     static PagerAdapter pagerAdapter;
